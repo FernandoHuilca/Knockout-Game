d8957496ed6919e5f75f1b434b8764a4eeae8f87
mateonicolasg <mateo.simbana@epn.edu.ec>
2024-12-21 21:11:47 -0500

    Adding Main Menu and character select menu

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void buttonStart()
+    {
+        SceneManager.LoadScene("CharacterSelectionMenu");
+    }
+}
