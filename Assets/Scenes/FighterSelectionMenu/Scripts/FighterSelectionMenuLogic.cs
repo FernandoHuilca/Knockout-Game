46e3d59ff759f447b7c6a1103118d86a3ed94476
FernandoHuilca <fernando.huilca@epn.edu.ec>
2025-01-16 21:48:06 -0500

    scenario one functional selection

--- Assets/Scenes/FighterSelectionMenu/Scripts/FighterSelectionMenuLogic.cs
+++ Assets/Scenes/FighterSelectionMenu/Scripts/FighterSelectionMenuLogic.cs
@@ -127,7 +127,8 @@ public class FighterSelectionMenuLogic : MonoBehaviour
     // ------------------------------------------------------------------------------------------------------------------------------------------
     public void startGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("ScenarySelectionMenu");
     }
 }
 
