2e606c6f69190396ba4190c5bad467b6e285daae
FernandoHuilca <fernando.huilca@epn.edu.ec>
2025-02-01 15:05:11 -0500

    Fixing sounds problems

--- Assets/Scenes/LeaderboardMenu/Scripts/LeaderboardManager.cs
+++ Assets/Scenes/LeaderboardMenu/Scripts/LeaderboardManager.cs
@@ -135,12 +135,13 @@ public class LeaderboardManager : MonoBehaviour
     // Esta función es llamada cuando se presiona un botón específico
     public void LoadSceneByButton(string sceneName)
     {
-        // Detener la música si es una escena de pelea
+        
+        /*// Detener la música si es una escena de pelea
         if (sceneName != "FighterSelectionMenu")
         {
             if (MenuMusicManager.Instance != null)
                 MenuMusicManager.Instance.StopMusic();
-        }
+        }*/
 
         SceneManager.LoadScene(sceneName);
     }
